bd7254b3ba2a861e6014e1e5bbd7c2f6e21009f4
nh-g <giangngohong@gmail.com>
2021-10-07 13:40:11 +0200

    add navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import Navigation from "./component/Navigation";
+import './styles/style.sass'
 export default function App() {
   return (
     <div className="App">
+      {/* Header */}
+      <Navigation/>
+      {/* Home */}
     </div>
+    
   );
 }
---
+++ src/component/Navigation.jsx
@@ -0,0 +1,37 @@
+// NPM Packages
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+
+//Project files
+import flameIcon from '../asset/images/brand/flame.svg';
+
+export default function NavigationBar() {
+  return (
+    <nav id="navigation-bar">
+      <h1>
+        <img src={flameIcon} alt="" style={{ height: "30px" }} />
+        Fireup
+      </h1>
+      <div className="flexbox-expand-space" />
+
+      {/* <SearchBar /> */}
+      <div className="search">
+        <input className="searchInput" type="text" />
+        <SearchIcon className="searchIcon" />
+      </div>
+
+      <div className="flexbox-expand-space" />
+
+      <div className="right-items">
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Sign In</span>
+        </div>
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
+        </div>
+      </div>
+    </nav>
+  );
+}
