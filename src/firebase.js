a9d8b7724f971c53fc188a426be6461d0a715ed3
nh-g <giangngohong@gmail.com>
2021-10-07 11:32:31 +0200

    initial setup react project

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAfGq7M0B2NxNZLGhdfCi2WTGZacxUFcX8",
+  authDomain: "ecommerce-giang.firebaseapp.com",
+  projectId: "ecommerce-giang",
+  storageBucket: "ecommerce-giang.appspot.com",
+  messagingSenderId: "721043646639",
+  appId: "1:721043646639:web:603bc36e7f1d593bae0c5a",
+};
