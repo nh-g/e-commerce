047469fcde20200f9c0f97363a629f411ef8dc33
nh-g <giangngohong@gmail.com>
2021-10-13 00:29:03 +0200

    render OK firebase data

--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -1,11 +1,17 @@
+// Project files
+import useFetch from '../../hooks/useFetch';
 import categoriesData from './categories.json';
 import CategoryMasonry from './CategoryMasonry';
 import MappingList from '../../shared/MappingList';
 export default function Menu() {
-    return (
-      <div className="menu">
-        <h1>MENU</h1>
-        <MappingList getData={categoriesData} Component={CategoryMasonry} />
-      </div>
-    );
+  // Hooks
+  const categoriesFireBase = useFetch("categories");
+  const categoriesData = categoriesFireBase.data;
+
+  return (
+    <div className="menu">
+      <h1>MENU</h1>
+      <MappingList getData={categoriesData} Component={CategoryMasonry} />
+    </div>
+  );
 }
