5444c859b96d7dd329f68be94cf58bf7c4088542
nh-g <giangngohong@gmail.com>
2021-10-15 05:58:10 +0200

    replace new configs because ran out of firestore quota daily usage

--- src/scripts/firebase/firebase.js
+++ src/scripts/firebase/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore/lite";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAfGq7M0B2NxNZLGhdfCi2WTGZacxUFcX8",
-  authDomain: "ecommerce-giang.firebaseapp.com",
-  projectId: "ecommerce-giang",
-  storageBucket: "ecommerce-giang.appspot.com",
-  messagingSenderId: "721043646639",
-  appId: "1:721043646639:web:603bc36e7f1d593bae0c5a",
+  apiKey: "AIzaSyCd9yH3Iyhb_sTSC2pTBQVHBZzqKWZHjTI",
+  authDomain: "ecom-admin-giang.firebaseapp.com",
+  projectId: "ecom-admin-giang",
+  storageBucket: "ecom-admin-giang.appspot.com",
+  messagingSenderId: "926714992601",
+  appId: "1:926714992601:web:20589a2ef1617b4029ebc8",
 };
 
 const firebaseInstance = initializeApp(firebaseConfig);
