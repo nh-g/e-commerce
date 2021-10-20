d34a0224359a270dd9f74d93aef5612cb8f85118
nh-g <giangngohong@gmail.com>
2021-10-20 16:13:37 +0200

    formating

--- src/pages/admin/Edit.jsx
+++ src/pages/admin/Edit.jsx
@@ -1,36 +1,30 @@
+// NPM Packages
 import { useCallback, useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
 
+// Project files
 import { getDocument, updateDocument } from "../../scripts/firebase/fireStore";
-import ProductEditor from "./FormProductEditor";
 
-export default function EditProduct({FormEditor}) {
+export default function EditProduct({ FormEditor }) {
   const history = useHistory();
 
-  console.log("history", history)
+  console.log("history", history);
   const [item, setItem] = useState();
   const [status, setStatus] = useState(0); // 0: loading, 1: loaded, 2: error
   const { productID: itemID } = useParams();
   const path = "products";
-  const fetchData = useCallback(
-    async (path) => {
+  const fetchData = useCallback(async (path) => {
       try {
         const selectedItem = await getDocument(path, itemID);
-        console.log("Edit Product Page", selectedItem);
-
         setItem(selectedItem);
         setStatus(1);
       } catch {
         setStatus(2);
       }
-    },
-    [itemID]
+    },[itemID]
   );
 
-  useEffect(() => {
-    fetchData(path);
-  }, [fetchData, path]);
-
+  useEffect(() => {fetchData(path);}, [fetchData, path]);
 
   async function onUpdate(selectedItem) {
     await updateDocument(path, selectedItem.id, selectedItem);
@@ -45,8 +39,11 @@ export default function EditProduct({FormEditor}) {
       {status === 1 && (
         <>
           <FormEditor onUpdate={onUpdate} item={item} />
-          <button className="button-secondary" onClick={() => history.goBack()}>
-            Go back
+          <button
+            className="btn btn-main btn-300"
+            onClick={() => history.goBack()}
+          >
+            Cancel
           </button>
         </>
       )}
