03f1862924488dc06717a418bc94b47bc5cbce33
nh-g <giangngohong@gmail.com>
2021-10-21 14:31:00 +0200

    add getRandom method to pick up 6 random products to display in home container

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,33 +1,48 @@
 import MappingList from "../../shared/MappingList.jsx";
-import homeProductData from "./products.json"; 
+import homeProductData from "./products.json";
 import Hero from "./Hero.jsx";
-import HomeProduct from '../category/ProductCard'
-
+import HomeProduct from "../category/ProductCard";
+import useFetch from "../../hooks/useFetch.js";
 export default function HomePage() {
-  const homeRow_1 = homeProductData.slice(0,2);
-  const homeRow_2 = homeProductData.slice(2,5);
-  const homeRow_3 = homeProductData.slice(homeProductData.length-1);
+  const products = useFetch("products");
+  const featuredProduct = getRandom(products.data, 6);
+
+
+  function getRandom(array, length) {
+    // safeguard
+    if (array.length < length) {
+      console.warn("You are trying to get more items than available");
+      length = array.length;
+    }
+    const clonedArray = [...array];
+    const shuffled = clonedArray.sort(() => 0.5 - Math.random());
+    const randomCandidates = shuffled.slice(0, length);
+
+    return randomCandidates;
+  }
+
+  const homeRow_1 = featuredProduct.slice(0, 2);
+  const homeRow_2 = featuredProduct.slice(2, 5);
+  const homeRow_3 = featuredProduct.slice(featuredProduct.length - 1);
 
   return (
     <>
-    <Hero />
-    <div className="home">
-      <div className="spacer intro" />
+      <Hero />
+      <div className="home">
+        <div className="spacer intro" />
 
-      {/* <div className="home__container">
-
-        <div className="home__row">
-          <MappingList getData={homeRow_1} Component={HomeProduct} />
+        <div className="home__container">
+          <div className="home__row">
+            <MappingList getData={homeRow_1} Component={HomeProduct} />
+          </div>
+          <div className="home__row">
+            <MappingList getData={homeRow_2} Component={HomeProduct} />
+          </div>
+          <div className="home__row">
+            <MappingList getData={homeRow_3} Component={HomeProduct} />
+          </div>
         </div>
-        <div className="home__row">
-          <MappingList getData={homeRow_2} Component={HomeProduct} />
-        </div>
-        <div className="home__row">
-          <MappingList getData={homeRow_3} Component={HomeProduct} />
-        </div>
-        
-      </div> */}
-    </div>
+      </div>
     </>
   );
 }
